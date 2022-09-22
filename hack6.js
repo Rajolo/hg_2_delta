a8008fe4cf3e77e4063cc856fffa5be2e516e1fc
rajolo <rafaeljoseloes@gmail.com>
2022-09-22 00:58:22 -0400

    feat: hacks realizados

--- hack6.js
+++
@@ -1,6 +0,0 @@
-//HACK-6 
-//total item
-
-let arr_h6 = [100,200,300,400,500,600,700];
-
-console.log(arr_h6.length)
--- hack7.js
+++
@@ -1,8 +0,0 @@
-//HACK-7 
-//remplazar el item 300 por tú alias
-
-let arr_h7 = [100,200,300,400,500,600,700];
-
-arr_h7.splice(2,1,"delta");
-
-console.log(arr_h7);
